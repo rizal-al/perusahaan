022f7e11e0393502de35098669523e347b7eb488
rizal-al <dhosting7@gmail.com>
2026-05-30 15:20:51 +0700

    Ubah tampilan homepage

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -504,63 +504,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 </script>
-<script>
-const canvas = document.getElementById('rain');
-const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// ================= HUJAN =================
-let drops = [];
-
-for(let i = 0; i < 200; i++){
-    drops.push({
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        length: Math.random() * 20 + 10,
-        speed: Math.random() * 5 + 5
-    });
-}
-
-// ================= DRAW =================
-function draw(){
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // HUJAN
-    ctx.strokeStyle = 'rgba(174,194,224,0.5)';
-    ctx.lineWidth = 1;
-
-    drops.forEach(d => {
-
-        ctx.beginPath();
-        ctx.moveTo(d.x, d.y);
-        ctx.lineTo(d.x, d.y + d.length);
-        ctx.stroke();
-
-        d.y += d.speed;
-
-        if(d.y > canvas.height){
-            d.y = -20;
-            d.x = Math.random() * canvas.width;
-        }
-
-    });
-
-    requestAnimationFrame(draw);
-}
-
-draw();
-
-// RESPONSIVE
-window.addEventListener('resize', () => {
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-});
-</script>
 <script>
 document.addEventListener("DOMContentLoaded", function(){
 
